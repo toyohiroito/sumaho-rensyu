cfb9696e0f1c67b75ed29945a127394759e5e1ca
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-03-31 08:43:38 +0900

    apt-pc sidebar.php変更

---
+++ html/apt-pc/sidebar.php
@@ -0,0 +1,11 @@
+    <div id="sidebar">
+      <div class="rss_link">
+        <a href="#"><img src="<?php bloginfo('template_url') ?>/images/ico_rss.png" width="215" height="29" alt="こちらのRSSを購読する。" /></a>
+      </div>
+      <div class="textwidget">
+        <p>
+          <a href="#"><img src="<?php bloginfo('template_url') ?>/images/bnr_blog.jpg" class="" width="215" height="110" alt="社長ブログ" />ジャカルタで働く社長のブログ。</a>
+        </p>
+      </div>
+      <img src="<?php bloginfo('template_url') ?>/images/sns.png" alt="" />
+    </div><!-- end #sidebar -->
