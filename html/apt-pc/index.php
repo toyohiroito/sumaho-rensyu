99da115a96786ffa15b726c4259d46e24a2917f0
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-02 09:34:58 +0900

    template分離

--- html/apt-pc/index.php
+++ html/apt-pc/index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8" />
-<title>アジアパシフィック旅行社</title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url') ?>" />
-<link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/images/favicon.ico" />
-<link rel="apple-touch-icon-precomposed" href="<?php bloginfo('template_url') ?>/images/apple-touch-icon.png" />
-</head>
-
-<body class="home">
-<div id="wrap">
-  <div id="header" role="banner">
-    <div class="inner clearfix">
-      <h1 class="site-id">
-        <a href="#"><img src="<?php bloginfo('template_url') ?>/images/site_id.png" width="322" height="42" alt="魅力あふれるアジア・太平洋の旅ならアジアパシフィック旅行社" /></a>
-      </h1>
-      <div class="utility">
-        <ul>
-          <li><a href="#">採用情報</a></li>
-        </ul>
-        <div id="search" role="search">
-          <form role="search" method="get" id="searchform" class="searchform" action="#">
-            <div>
-              <input type="text" value="" name="s" id="s" />
-              <input type="submit" id="searchsubmit" value="検索" />
-            </div>
-          </form>          
-        </div><!-- #search end -->
-      </div><!-- .utility end -->
-    </div><!-- .inner end -->
-    <div id="global_nav">
-      <ul>
-        <li class="current-menu-item menu-item-slug-top"><a href="#">トップページ</a></li>
-      </ul>
-    </div>
-  </div><!-- #header end -->
+<?php get_header(); ?>
   <div class="main_image">
     <a href="#"><img src="<?php bloginfo('template_url') ?>/images/top/main_image.png" width="950" height="329" alt="想像以上の旅をご提供いたします。" /></a>
   </div><!-- .main_image end -->
@@ -82,51 +46,6 @@
         </div><!-- end #topInfo -->
       </div><!-- end #content -->
     </div><!-- end #main -->
-    <div id="sidebar">
-      <div class="rss_link">
-        <a href="#"><img src="<?php bloginfo('template_url') ?>/images/ico_rss.png" width="215" height="29" alt="こちらのRSSを購読する。" /></a>
-      </div>
-      <div class="textwidget">
-        <p>
-          <a href="#"><img src="<?php bloginfo('template_url') ?>/images/bnr_blog.jpg" class="" width="215" height="110" alt="社長ブログ" />ジャカルタで働く社長のブログ。</a>
-        </p>
-      </div>
-      <img src="<?php bloginfo('template_url') ?>/images/sns.png" alt="" />
-    </div><!-- end #sidebar -->
+<?php get_sidebar(); ?>
   </div><!-- end #container -->
-</div><!-- #wrap end -->
-<div id="footer-container">
-  <div id="footer">
-    <div class="footer_menu">
-      <div class="footerBlock widget_nav_menu">
-        <h2><a href="#">ツアー情報</a></h2>
-        <ul>
-          <li><a href="#">ハワイ・グアム</a></li>
-        </ul>
-      </div>
-      <div class="footerBlock widget_nav_menu">
-        <h2><a href="#">営業所</a></h2>
-        <ul>
-          <li><a href="#">大手町営業所</a></li>
-        </ul>
-      </div>
-      <div class="footerBlock widget_nav_menu">
-        <h2><a href="#">お知らせ</a></h2>
-        <ul>
-         <li><a href="#">IR情報</a></li>
-        </ul>
-      </div>
-      <div class="footerBlock widget_nav_menu">
-        <h2><a href="#">会社概要</a></h2>
-        <ul>
-          <li><a href="#">事業紹介</a></li>
-        </ul>
-      </div>
-    </div><!-- #footer_menu end -->
-    <p id="copyright">
-      <small>Copyright &copy; アジアパシフィック旅行社 All rights reserved.</small>
-    </p>
-  </div><!-- #footer end -->
-</div><!-- #footer-container end -->
-</body>
-</html>
+<?php get_footer(); ?>
