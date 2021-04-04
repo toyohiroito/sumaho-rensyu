052b68be0d25dfa7f8938c2a6dc548fbc2ab919e
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-04 20:05:05 +0900

    page.php content追加

--- html/apt-pc/page.php
+++ html/apt-pc/page.php
@@ -2,7 +2,24 @@
   <div id="container">
     <div id="main" role="main">
       <div id="content">
-        <div class="clearfix" id="page_top_link"><a href="#wrap">このページの先頭へ</a></div>
+        <?php
+          if(have_posts()) :
+            while(have_posts()) :
+              the_post();
+        ?>
+        <h1 class="page_ttl">
+          <?php the_title(); ?>
+        </h1>
+        <?php the_content(); ?>
+        <?php
+            endwhile;
+          endif;
+        ?>
+        <div class="clearfix" id="page_top_link">
+          <a href="#wrap">
+            このページの先頭へ
+          </a>
+        </div>
       </div><!-- end #content -->
     </div><!-- end #main -->
 <?php get_sidebar(); ?>
