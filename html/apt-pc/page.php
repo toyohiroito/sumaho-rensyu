788ed1b8c6e370411a7f26e9332924499e2733ee
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-04 16:12:34 +0900

    index.phpをpage.phphe

---
+++ html/apt-pc/page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+  <div id="container">
+    <div id="main" role="main">
+      <div id="content">
+        <div class="clearfix" id="page_top_link"><a href="#wrap">このページの先頭へ</a></div>
+      </div><!-- end #content -->
+    </div><!-- end #main -->
+<?php get_sidebar(); ?>
+  </div><!-- end #container -->
+<?php get_footer(); ?>
