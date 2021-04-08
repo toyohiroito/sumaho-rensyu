c2d04fb05e9648ad3572860ec6bc495df597dc10
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-09 06:09:35 +0900

    sidebar.php HTMLからget_template_partに変更

--- html/apt-pc/sidebar.php
+++ html/apt-pc/sidebar.php
@@ -1,11 +1,5 @@
-    <div id="sidebar">
-      <div class="rss_link">
-        <a href="#"><img src="<?php bloginfo('template_url') ?>/images/ico_rss.png" width="215" height="29" alt="こちらのRSSを購読する。" /></a>
-      </div>
-      <div class="textwidget">
-        <p>
-          <a href="#"><img src="<?php bloginfo('template_url') ?>/images/bnr_blog.jpg" class="" width="215" height="110" alt="社長ブログ" />ジャカルタで働く社長のブログ。</a>
-        </p>
-      </div>
-      <img src="<?php bloginfo('template_url') ?>/images/sns.png" alt="" />
-    </div><!-- end #sidebar -->
+<div id="sidebar">
+  <?php
+    get_template_part('sidebar-common');
+  ?>
+</div><!-- end #sidebar -->
