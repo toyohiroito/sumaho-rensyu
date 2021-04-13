5b91bd0e022b553a62a07ff2043cd9fc616ad07a
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-13 20:23:32 +0900

    content-archive.php カテゴリーごとのテンプレート完成

---
+++ html/apt-pc/content-archive.php
@@ -0,0 +1,23 @@
+<?php
+  $cat_info = apt_category_info();
+?>
+<div class="content_excerpt">
+  <div class="news_headline clearfix">
+    <span class="news_date">
+      <?php
+        the_time('Y年m月d日');
+      ?>
+    </span>
+    <span class="news_category <?php echo esc_attr($cat_info->slug); ?>">
+      <?php
+        echo esc_html($cat_info->name); 
+      ?>
+    </span>
+  </div>
+  <h2 class="page_ttl">
+    <a href="<?php the_permalink(); ?>">
+      <?php the_title(); ?>
+    </a>
+  </h2>
+  <?php the_excerpt(); ?>
+</div><!-- end .content_excerpt -->
