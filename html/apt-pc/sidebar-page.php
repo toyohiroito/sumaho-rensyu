b789632bf36736a0c6265000e12ecbb1750cf898
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-13 17:27:10 +0900

    sidebar-page.php 実装

--- html/apt-pc/sidebar-page.php
+++ html/apt-pc/sidebar-page.php
@@ -1,4 +1,22 @@
+<?php
+  $anc = apt_page_ancestor();
+?>
 <div id="sidebar">
+  <div id="side_nav">
+    <h2>
+      <?php
+        echo esc_html($anc->post_title);
+      ?>
+    </h2>
+    <ul class="sub_navi">
+      <?php
+        wp_list_pages(array('include' => $anc->ID, 'title_li' => 0));
+      ?>
+      <?php
+        wp_list_pages(array('child_of' => $anc->ID, 'title_li' => 0, 'depth' => 2));
+      ?>
+    </ul>
+  </div>
   <?php
     get_template_part('sidebar-common');
   ?>
