8aa785ccd6c8810a6d26efaaf08e8f35d5a8e843
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-13 20:34:58 +0900

    archive.php 修正(記事を表示しないようにした)

--- html/apt-pc/archive.php
+++ html/apt-pc/archive.php
@@ -12,17 +12,6 @@
             while(have_posts()) :
               the_post();
               get_template_part('content-archive');
-        ?>
-        <h1 class="page_ttl">
-          <?php the_title(); ?>
-        </h1>
-          <div class="clearfix">
-            <span class="date">
-                <?php the_date('Y年m月d日'); ?>
-            </span>
-          </div>
-        <?php the_content(); ?>
-        <?php
             endwhile;
           endif;
           if(class_exists('WP_SiteManager_page_navi')) :
