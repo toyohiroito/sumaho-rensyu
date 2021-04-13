fa3a2e3109be2b7c6104a84a0955212c97e9d06d
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-13 19:34:52 +0900

    sidebar-post.php 作成

--- html/apt-pc/sidebar-post.php
+++ html/apt-pc/sidebar-post.php
@@ -1,19 +1,26 @@
 <?php
-  $anc = apt_page_ancestor();
+  $anc = apt_category_id();
 ?>
 <div id="sidebar">
   <div class="side_nav">
     <h2>
-      <?php
-        echo esc_html($anc->post_title);
-      ?>
+      お知らせ
     </h2>
     <ul class="sub_navi">
       <?php
-        wp_list_pages(array('include' => $anc->ID, 'title_li' => 0));
+        wp_list_pages(
+          array(
+          'include' => get_page_by_path('news')->ID,
+          'title_li' => 0
+        ));
       ?>
       <?php
-        wp_list_pages(array('child_of' => $anc->ID, 'title_li' => 0, 'depth' => 2));
+        wp_list_categories(
+          array(
+          'title_li' => false,
+          'hide_empty' => true,
+          'current_category' => $cat_id
+        ));
       ?>
     </ul>
   </div>
