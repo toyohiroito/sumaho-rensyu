05d4bd522addd68e0aaa47791d4de37fd5bbacb8
toyohiroito <63059319+toyohiroito@users.noreply.github.com>
2021-04-13 18:18:23 +0900

    single.php 作成

---
+++ html/apt-pc/single.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+  <div id="container">
+    <div id="main" role="main">
+      <div id="content">
+        <?php
+          if(have_posts()) :
+            while(have_posts()) :
+              the_post();
+        ?>
+        <h1 class="page_ttl">
+          <?php the_title(); ?>
+        </h1>
+        <?php the_content(); ?>
+        <?php
+            endwhile;
+          endif;
+        ?>
+        <?php
+          get_template_part('page_top_link');
+        ?>
+      </div><!-- end #content -->
+    </div><!-- end #main -->
+<?php get_sidebar('page'); ?>
+  </div><!-- end #container -->
+<?php get_footer(); ?>
